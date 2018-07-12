6430380672f372b66d62eb69b652f7f31a9e05bd
Полицинский Артем Сергеевич <as_policinskij@bingo-boom.ru>
2018-07-12 17:17:42 +0300

    - debug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 export class View {
 
-    constructor() {
+    constructor({ key, node, resources, pid, ...props } = {}) {
+        this.pid = pid;
+        this.target = node;
+    }
 
+    add(...args) {
+        this.target.appendChild(...args.map( ({ target }) => !target.parentNode && target ));
+    }
+
+    remove() {
+        this.target.remove();
     }
 
 }
